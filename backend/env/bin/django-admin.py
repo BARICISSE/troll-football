6bb05712bc8af993f22d0bf6a69786323ca28c77
BARICISSE <ousmane.baricisse@mail.mcgil.ca>
2019-05-11 03:12:13 -0400

    update project setup-frontend and backend setup

---
+++ backend/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ousmanebaricisse/Projects/troll-football/backend/env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
